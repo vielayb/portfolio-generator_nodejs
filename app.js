b443173afd1dd0d8d6a009640e1a37cad260ceb1
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-03-15 13:45:51 -0400

    modulized app

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs; //ES6 feature called assignment destructuring, it assigns elements of an array to variable names in a single expression
 
-
-
-
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+  //reference 9.2.5
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
   
-    console.log('================');
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
   
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
 
-    console.log('================');
-
-    //cleaner code
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-  };
-  
-  
-  printProfileData(profileDataArgs);
