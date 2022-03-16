d77f694ef65ff6a5715283ef843e6246071cdb4c
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-03-16 09:55:54 -0400

    inquirer installed

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs; //ES6 feature called assignment destructuring, it assigns elements of an array to variable names in a single expression
-
-  //reference 9.2.5
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//   //reference 9.2.5
+//   fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw (err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
   
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
